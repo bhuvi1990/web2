0e56c235f8295627a3ab01f3bb9d7d547cad2e91
bhuvi1990 <er.bhuvneshmalik@gmail.com>
2019-05-02 18:08:12 +0530

    post input setup

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.post('/',(req,res)=>{
+  var email = req.body.email;
+  var amount = req.body.amount;
+
+  res.send({"amount" : amount , "email" : email });
+})
+app.get('/',(req,res) =>{
+  res.send("HELLO WEB 2.0");
+})
+console.log('Hello web 2.0');
+
+app.listen(3000,()=>{
+  console.log('server is running on port 3000');
+});
